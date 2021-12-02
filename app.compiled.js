c459190a0041cb7029f60b5935d9960b8ca1e7d8
Andrew <afgasparo@gmail.com>
2021-12-02 15:18:44 -0500

    minor fixes

--- app.compiled.js
+++ app.compiled.js
@@ -11,6 +11,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join('public')));
 
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+//   next();
+// });
+
 // any route in post-routes will need this prefix
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
@@ -30,7 +37,7 @@ app.use((error, req, res, next) => {
 });
 
 const db = require("./app/models");
-db.mongoose.connect(db.url || db.connectionString, {
+db.mongoose.connect(db.url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
@@ -39,13 +46,12 @@ db.mongoose.connect(db.url || db.connectionString, {
   console.log("Cannot connect to the database!", err);
   process.exit();
 });
-mongoose.Promise = global.Promise;
 
 app.get("/", (req, res) => {
   res.send("welcome to the server");
 });
 
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
