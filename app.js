8ba95a8b370cf02f0070b2294b432c87e2da173a
Andrew <afgasparo@gmail.com>
2022-05-11 10:32:03 -0400

    removed more static app.use

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(cors());
 
 app.use(
   "/uploads/images",
-  express.static(path.join("public", "uploads", "images"));
+  express.static(path.join("uploads", "images"));
 );
 
-app.use(express.static(path.join("public")));
+//app.use(express.static(path.join("public")));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
